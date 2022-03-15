8a8bc53f8c36752678597e693c8460b9696998f6
Ernest Paul <ernestpaul88@gmail.com>
2022-03-15 11:44:49 -0400

    finished

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# Note Taker Starter Code
+# Note Taker
+
+## Description:
+
+A simple node application that can be used to write and save notes. This application uses an Express.js back end and saves and retrieves note data from a JSON file.
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Questions](#questions)
+
+## Installation:
+
+If you already have node installed the only step for installing the project is running 'npm install' from the root directory.
+
+## Usage:
+
+To start the project you need to run 'npm start' from the root directory.
+
+## Questions:
+
+- You can email me at ernestpaul88@gmail.com if you have any questions.
+- Github: [Ernest Paul Github](https://github.com/ernestpaul88)
---
+++ routes/apiRoutes/index.js
@@ -0,0 +1,30 @@
+const router = require("express").Router();
+const { createNote, deleteNoteById, validateNote } = require("../../lib/notes");
+const { notes } = require("../../db/db.json");
+const uniqid = require("uniqid");
+
+router.get("/notes", (req, res) => {
+  res.json(notes);
+});
+
+router.delete("/notes/:id", (req, res) => {
+  notes = deleteNoteById(req.params.id, notes);
+  if (notes) {
+    res.json(notes);
+  } else {
+    res.send(404);
+  }
+});
+
+router.post("/notes", (req, res) => {
+  req.body.id = uniqid();
+
+  if (!validateNote(req.body)) {
+    res.status(400).send("The note is not properly formatted.");
+  } else {
+    const note = createNote(req.body, notes);
+    res.json(note);
+  }
+});
+
+module.exports = router;
